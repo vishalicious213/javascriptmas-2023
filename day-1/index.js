9091a9e117b7d01a5037dd3d30a385808482182a
vishalicious213 <vishalicious213@gmail.com>
2023-12-02 12:57:32 -0800

    adjusted test date back to christmas date

--- day-1/index.js
+++ day-1/index.js
@@ -2,7 +2,7 @@ const countdownDisplay = document.getElementById("countdown-display")
 const countdownDetails = document.getElementById("countdown-details")
 
 function renderCountdown(){
-    const christmas = new Date("Dec 02, 2023 00:00:00")
+    const christmas = new Date("Dec 25, 2023 00:00:00")
     const date = new Date()
 
     let day = christmas.getDate() - date.getDate()
@@ -12,7 +12,7 @@ function renderCountdown(){
 function updateCountdown() {
     const now = new Date()
 
-    if (now.getMonth() >= 11 && now.getDate() >= 2) {
+    if (now.getMonth() >= 11 && now.getDate() >= 25) {
         countdownDetails.innerText = "2023 Christmas is done!"
     } else {
         countdownDetails.innerText = `${24 - now.getHours()} : ${60 - now.getMinutes()} : ${60 - now.getSeconds()}`
