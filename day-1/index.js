45f36e0bd84249ba9fd4652713febb80b7946594
vishalicious213 <vishalicious213@gmail.com>
2023-12-02 11:54:57 -0800

    updated /day-1/index.js to show days until christmas

--- day-1/index.js
+++ day-1/index.js
@@ -4,8 +4,11 @@ function renderCountdown(){
     const christmas = 25
     // Task:
     // - Get today's date (you only need the day).
+    const date = new Date()
     // - Calculate remaining days.
+    let day = christmas - date.getDate()
     // - Display remaining days in countdownDisplay.
+    countdownDisplay.innerText = day
 }
  
 renderCountdown()
