eebb5254dcdd88cfe962f41c70f26f735aec80e3
vishalicious213 <vishalicious213@gmail.com>
2023-12-02 12:55:28 -0800

    changed updateCountdown() render into an if statement that accounts for christmas passing

--- day-1/index.js
+++ day-1/index.js
@@ -2,7 +2,7 @@ const countdownDisplay = document.getElementById("countdown-display")
 const countdownDetails = document.getElementById("countdown-details")
 
 function renderCountdown(){
-    const christmas = new Date("Dec 25, 2023 00:00:00")
+    const christmas = new Date("Dec 02, 2023 00:00:00")
     const date = new Date()
 
     let day = christmas.getDate() - date.getDate()
@@ -11,7 +11,12 @@ function renderCountdown(){
 
 function updateCountdown() {
     const now = new Date()
-    countdownDetails.innerText = `${24 - now.getHours()} : ${60 - now.getMinutes()} : ${60 - now.getSeconds()}`
+
+    if (now.getMonth() >= 11 && now.getDate() >= 2) {
+        countdownDetails.innerText = "2023 Christmas is done!"
+    } else {
+        countdownDetails.innerText = `${24 - now.getHours()} : ${60 - now.getMinutes()} : ${60 - now.getSeconds()}`
+    }
 }
 
 renderCountdown()
