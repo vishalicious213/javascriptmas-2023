b2630a412017ee61169f32e2aa6ec9e8f849ff88
vishalicious213 <vishalicious213@gmail.com>
2023-12-02 12:31:50 -0800

    running a function (updateCountdown) every second

--- day-1/index.js
+++ day-1/index.js
@@ -2,16 +2,18 @@ const countdownDisplay = document.getElementById("countdown-display")
 
 function renderCountdown(){
     const christmas = new Date("Dec 25, 2023 00:00:00")
-    // Task:
-    // - Get today's date (you only need the day).
     const date = new Date()
-    // - Calculate remaining days.
+
     let day = christmas.getDate() - date.getDate()
-    // - Display remaining days in countdownDisplay.
     countdownDisplay.innerText = day
 }
 
+function updateCountdown() {
+ console.log(new Date())
+}
+
 renderCountdown()
+setInterval(updateCountdown, 1000)
 
 // Stretch goals:
 // - Display hours, minutes, seconds.
