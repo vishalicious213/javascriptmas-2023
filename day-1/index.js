cc3f82ef22376bddaab92dde152fa07181092180
vishalicious213 <vishalicious213@gmail.com>
2023-12-02 12:26:01 -0800

    updated christmas from number to a specific Date(). updated day to use updated value in calculation

--- day-1/index.js
+++ day-1/index.js
@@ -1,12 +1,12 @@
 const countdownDisplay = document.getElementById("countdown-display")
 
 function renderCountdown(){
-    const christmas = 25
+    const christmas = new Date("Dec 25, 2023 00:00:00")
     // Task:
     // - Get today's date (you only need the day).
     const date = new Date()
     // - Calculate remaining days.
-    let day = christmas - date.getDate()
+    let day = christmas.getDate() - date.getDate()
     // - Display remaining days in countdownDisplay.
     countdownDisplay.innerText = day
 }
