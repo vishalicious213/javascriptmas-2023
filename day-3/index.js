9218265d18bc619c4e7c8938637b46a8d3350449
vishalicious213 <vishalicious213@gmail.com>
2023-12-02 23:05:46 -0800

    /day-3: solved math part of challenge

--- day-3/index.js
+++ day-3/index.js
@@ -13,9 +13,11 @@ function calcTotalCandies(children, candy) {
      * is 3*3 = 9. So the function calcTotalCandies should 
      * log out 9.
      **/
+
+    let totalCandiesEaten = Math.floor(candy / children) * children
     console.log(totalCandiesEaten)
     }
     
-    calcTotalCandies(3, 10) // expected output: 9
-    calcTotalCandies(4, 20) // expected output: 20
-    calcTotalCandies(6, 25) // expected output: 24
+calcTotalCandies(3, 10) // expected output: 9
+calcTotalCandies(4, 20) // expected output: 20
+calcTotalCandies(6, 25) // expected output: 24
